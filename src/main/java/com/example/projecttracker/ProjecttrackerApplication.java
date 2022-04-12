c40812237ef20dd2493043a27c9aedfd5851a0ba
Steph Paterson <steph.c.paterson@gmail.com>
2022-04-12 10:25:04 +0100

    initial file setup

---
+++ src/main/java/com/example/projecttracker/ProjecttrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.projecttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecttrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecttrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projecttracker/ProjecttrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projecttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecttrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
